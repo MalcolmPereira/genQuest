07fc2fbc94a884a19b241dbebeffb93ddac956c6
Malcolm Pereira <malcolm.j.pereira@gmail.com>
2014-07-27 20:37:25 -0400

    updates for select all javascript

--- public/javascripts/genQuest.js
+++ public/javascripts/genQuest.js
@@ -6,3 +6,17 @@ function clearLoginError() {
         div.style.display = 'block';
     }
 }
+
+$(document).ready(function() {
+    $('#selecctall').click(function(event) {  //on click 
+        $('.checkbox1').each(function() { //loop through each checkbox
+                this.checked = true;  //select all checkboxes with class "checkbox1"               
+        });
+    });
+    $('#unselecctall').click(function(event) {  //on click 
+        $('.checkbox1').each(function() { //loop through each checkbox
+                this.checked = false;  //select all checkboxes with class "checkbox1"               
+        });
+    });
+	
+});
